db8a79e132af42b3b4fa69797ff0ef3f6c8c01bb
SP Mohanty <spmohanty91@gmail.com>
2019-07-02 16:38:18 +0200

    Remove references to PILSVG from all user facing examples

--- examples/tkplay.py
+++ examples/tkplay.py
@@ -4,13 +4,16 @@ from flatland.envs.rail_env import RailEnv
 from flatland.utils.rendertools import RenderTool
 
 
-def tkmain(n_trials=2, n_steps=50, sGL="PIL"):
+def tkmain(n_trials=2, n_steps=50):
     # Example generate a random rail
     env = RailEnv(width=15, height=15,
-                  rail_generator=complex_rail_generator(nr_start_goal=5, nr_extra=20, min_dist=12),
+                  rail_generator=complex_rail_generator(
+                                    nr_start_goal=5,
+                                    nr_extra=20,
+                                    min_dist=12),
                   number_of_agents=5)
 
-    env_renderer = RenderTool(env, gl=sGL)
+    env_renderer = RenderTool(env)
 
     oPlayer = Player(env)
     n_trials = 1
@@ -29,5 +32,4 @@ def tkmain(n_trials=2, n_steps=50, sGL="PIL"):
 
 
 if __name__ == "__main__":
-    tkmain(sGL="PIL")
-    tkmain(sGL="PILSVG")
+    tkmain()
